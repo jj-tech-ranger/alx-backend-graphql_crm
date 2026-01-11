0d9bf18901bdc82e65085f11d6954851215fa1c9
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-01-11 14:02:16 +0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -1,9 +1,28 @@
-INSTALLED_APPS = [
-    'django_crontab', #
-]
+import os
+from celery.schedules import crontab
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'django_crontab',
+    'django_celery_beat',
+    'crm',
+]
 
 CRONJOBS = [
-    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'), 
-    ('0 */12 * * *', 'crm.cron.update_low_stock'), 
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
