f4b8efe30badca01b8c9a7de097c0dec57eea66d
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-01-11 13:40:21 +0300

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -1,24 +1,31 @@
 import graphene
-from .models import Product
+from graphene_django import DjangoObjectType
+from crm.models import Product  #
 
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
 
 class UpdateLowStockProducts(graphene.Mutation):
     updated_products = graphene.List(graphene.String)
     message = graphene.String()
 
     def mutate(self, info):
+        # Query products with stock < 10
         low_stock_items = Product.objects.filter(stock__lt=10)
         names = []
         for product in low_stock_items:
-            product.stock += 10  # Simulating restocking
+            product.stock += 10
             product.save()
             names.append(product.name)
-
+        
         return UpdateLowStockProducts(
             updated_products=names,
             message="Stock successfully updated for low-stock products."
         )
 
-
 class Mutation(graphene.ObjectType):
     update_low_stock_products = UpdateLowStockProducts.Field()
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello!")
