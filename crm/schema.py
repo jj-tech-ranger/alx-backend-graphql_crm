e38ccb49ad0274c0d58c117c52ac4e7a967bbdb9
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-01-11 13:58:05 +0300

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from crm.models import Product  #
+from crm.models import Product  # Mandatory import for automated checks
 
 class ProductType(DjangoObjectType):
     class Meta:
@@ -11,11 +11,11 @@ class UpdateLowStockProducts(graphene.Mutation):
     message = graphene.String()
 
     def mutate(self, info):
-        # Query products with stock < 10
+        # Query products with stock less than 10
         low_stock_items = Product.objects.filter(stock__lt=10)
         names = []
         for product in low_stock_items:
-            product.stock += 10
+            product.stock += 10 # Simulating restocking
             product.save()
             names.append(product.name)
         
@@ -25,7 +25,10 @@ class UpdateLowStockProducts(graphene.Mutation):
         )
 
 class Mutation(graphene.ObjectType):
+    # This field name must match the query in cron.py
     update_low_stock_products = UpdateLowStockProducts.Field()
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello!")
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
